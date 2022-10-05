88f87aa270bd4a9415313fba9e3765b109b38ecb
Pwshady <pwshady@gmail.com>
2022-10-05 21:31:22 +0300

    start

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
